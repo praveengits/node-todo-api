021e54902344dcbecf8a6c1f089041a70a70c618
praveengits <praveenhears@gmail.com>
2018-02-13 15:34:27 -0600

    configuring mongo and heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://praveen_r08:United25@ds255347.mlab.com:55347/todoApp');
+mongodb://<dbuser>:<dbpassword>@ds255347.mlab.com:55347/todo
 mongoose.exports = {
     mongoose
 };
